9aeee139f26a8302cab7f7c7b934c90051b3ae0d
xxLucaa14 <66169446+xxLucaa14@users.noreply.github.com>
2020-07-01 19:17:17 +0200

    Add files via upload

---
+++ src/xxLucaa14/Main.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace xxLucaa14;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Level\LevelTimings;
+use pocketmine\Level;
+
+class Main extends PluginBase{
+    public function onEnable()
+    {
+        $this->getLogger()->info("Plugin wurde erfolgreich aktiviert..");
+    }
+    public function onDisable()
+    {
+        $this->getLogger()->info("Plugin wurde erfolgreich deaktiviert..");
+    }
+
+    public function onCommand(CommandSender $player, Command $command, string $label, array $args): bool
+    {
+
+        switch ($command->getName()) {
+            case "day":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("day.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast §6Tag §fgemacht.");
+                        $player->getLevel()->setTime(6000);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "night":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("night.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast §6Nacht §fgemacht.");
+                        $player->getLevel()->setTime(14000);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "feed":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("feed.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDein Hunger wurde geheilt.");
+                        $player->setFood(20);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "heal":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("heal.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDeine Herzen wurden geheilt.");
+                        $player->setHealth(20);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "gm 0":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("gm.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDein Gamemode wurde zu §6überleben §fgewechselt.");
+                        $player->setGamemode(0);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "gm 1":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("gm.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDein Gamemode wurde zu §6kreativ §fgewechselt.");
+                        $player->setGamemode(1);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "gm 2":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("gm.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDein Gamemode wurde zu §6abenteuer §fgewechselt.");
+                        $player->setGamemode(2);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+
+        switch ($command->getName()) {
+            case "gm 3":
+                if ($player instanceof Player){
+                    if ($player->hasPermission("gm.command.sc")){
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDein Gamemode wurde zu §6zuschauer §fgewechselt.");
+                        $player->setGamemode(3);
+                    } else {
+                        $player->sendMessage("§l§6ServerCore §8| §r§fDu hast keine Rechte diesen Command auszuführen!");
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+}
